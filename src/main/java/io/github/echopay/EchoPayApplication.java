73d385bb47f7c646a88e075958167630c7dabf98
Kaique de Miranda <kbdemiranda@hotmail.com>
2024-10-23 11:45:36 -0300

    Iniciando o Desenvolvimento

---
+++ src/main/java/io/github/echopay/EchoPayApplication.java
@@ -0,0 +1,13 @@
+package io.github.echopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/echopay/EchoPayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.echopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
